f8dce358f46e89acff2a40aad534111ae66827b7
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-15 22:20:06 +0100

    Added customer to the database

--- resources/views/internals/customers.blade.php
+++ resources/views/internals/customers.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <h1>Customers</h1>
 
+    <form action="customers" method="POST" class="pb-5">
+        <div class="input-group">
+            <input type="text" name="name">
+        </div>
+
+        <button type="submit">Add Customer</button>
+        {{ csrf_field() }}
+    </form>
     <ul>
         @foreach($customers as $customer)
             <li>{{ $customer->name }}</li>
