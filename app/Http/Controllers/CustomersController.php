e5e1d063a2715d6c5c644c663bd694feaa2bfec3
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-15 22:19:46 +0100

    Added customer to the database

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -14,4 +14,13 @@ class CustomersController extends Controller
 
         return view('internals.customers', compact('customers'));
     }
+
+    public function store()
+    {
+        $customer = new Customer();
+        $customer->name = request('name');
+        $customer->save();
+
+        return back();
+    }
 }
