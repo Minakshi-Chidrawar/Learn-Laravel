43bdc7747a9b0f36ecb8557b3004cf95673f98a0
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-16 16:02:47 +0100

    Nav is splitted

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,29 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Learn Laravel</title>
+        <title>@yield('title', 'Learning Laravel')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="about">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="contact">Contact Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="customers">Customer List</a>
-            </li>
-        </ul>
-
         <div class="container">
+            @include('nav')
             @yield('content')
         </div>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
