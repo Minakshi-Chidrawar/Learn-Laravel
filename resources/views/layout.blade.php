84dfb7b7a25b1ee14964bdd425f24f2afb0b7105
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2019-06-27 05:00:34 +0100

    Amended to display session message if it has

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,13 @@
     <body>
         <div class="container">
             @include('nav')
+
+            @if(session()->has('message'))
+                <div class="alert alert-success" role="alert">
+                    <strong>Success:</strong> {{ session()->get('message') }}
+                </div>
+            @endif
+
             @yield('content')
         </div>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
